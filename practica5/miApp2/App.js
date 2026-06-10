8d85c43b5bebf6f164f7617ddb20fa3141facf03
LuisEduardoSantanoDelgado <123046272@upq.edu.mx>
2026-06-09 22:33:31 -0600

    Practica 6 Tarea/Perfil.js

--- practica5/miApp2/App.js
+++ practica5/miApp2/App.js
@@ -2,26 +2,32 @@
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {Saludo} from './components/Saludo';
-import {Salu2} from './components/Salu2';                
+import { Saludo } from './components/Saludo';
+import { Salu2 } from './components/Salu2';
+import { Perfil } from './components/Perfil';
 
 /* Zona 2: Main - Hogar de los componentes */
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>--------------Componente nativo--------------</Text>
 
-      <Text>--------------Componentea nativos--------------</Text>
       <Text>Hola Mundo</Text>
-      <Image source={require('./assets/wave.png')}/>
+
+      <Image
+        source={require('./assets/wave.png')}
+        style={styles.image}
+      />
 
       <Text>--------------Componente simple--------------</Text>
-
       <Saludo />
 
-       <Text>--------------Componente Propio--------------</Text>
-
+      <Text>--------------Componente propio--------------</Text>
       <Salu2 />
+
+      <Text>--------------Perfil--------------</Text>
+      <Perfil />
 
       <StatusBar style="auto" />
     </View>
@@ -37,4 +43,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
---
+++ practica5/miApp2/components/Perfil.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+export const Perfil = () => {
+  return (
+    <View>
+      <Text>Nombre: Santano Delgado Luis Eduardo</Text>
+      <Text>Carrera: Sistemas Computacionales</Text>
+      <Text>Materia: Programación Movil</Text>
+      <Text>Cuatrimestre: 9</Text>
+    </View>
+  );
+};
+
